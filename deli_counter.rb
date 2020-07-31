e9f0d2a8be8151cf4ef48dd34c70dff432991b02
Marques Avery <averymarques4@gmail.com>
2020-07-31 18:21:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def line(katz_deli)
+  line = []
+  katz_deli.each_with_index do |queue, index|
+    index += 1
+    line << "#{index}. #{queue}"
+  end
+  return "The line is currently #{}"
