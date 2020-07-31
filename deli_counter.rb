f7c84c4118800cbc4c80ca73b2692d6af99d072b
Marques Avery <averymarques4@gmail.com>
2020-07-31 18:59:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, customer)
-  binding.pry
+#  binding.pry
   katz_deli << customer
 #  order = katz_deli.length
   puts "Welcome, #{customer}. You are number #{katz_deli.length} in line"
